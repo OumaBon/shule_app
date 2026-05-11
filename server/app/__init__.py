417d4b873cbf7c71caf93b262d84a9bdd8241fdf
OumaBon <mugahboniface@gmail.com>
2026-05-11 21:51:40 +0300

    flask application update

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy 
 from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
 from config import config
 
 
@@ -8,6 +9,8 @@ from config import config
 
 db = SQLAlchemy() 
 jwt = JWTManager()
+bcrypt = Bcrypt()
+
 
 
 def create_app(config_name):
@@ -17,6 +20,8 @@ def create_app(config_name):
 
     db.init_app(app) 
     jwt.init_app(app)
+    bcrypt.init_app(app)
+  
     
     from app.api_v1 import api 
     app.register_blueprint(api)
