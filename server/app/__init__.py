164b51973363e813b84c9ec8627fbbce2075ee51
OumaBon <mugahboniface@gmail.com>
2026-05-11 17:12:53 +0300

    application entry feature

---
+++ server/app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy 
+from flask_jwt_extended import JWTManager
+from config import config
+
+
+
+
+db = SQLAlchemy() 
+jwt = JWTManager()
+
+
+def create_app(config_name):
+    app = Flask(__name__) 
+    app.config.from_object(config[config_name]) 
+    config[config_name].init_app(app)
+
+    db.init_app(app) 
+    jwt.init_app(app)
+    
+    from app.api_v1 import api 
+    app.register_blueprint(api)
+
+
+    return app  
